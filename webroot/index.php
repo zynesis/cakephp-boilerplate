b1babc5ca637b66effac7e5b57c693cac61f30d8
U-Zyn Chua <chua@uzyn.com>
2013-08-06 16:05:12 +0800

    CakePHP fix
    https://github.com/cakephp/cakephp/commit/c28cde4e56304d6f9c85c504a6c23c
    147a63059c

--- webroot/index.php
+++ webroot/index.php
@@ -85,7 +85,7 @@ if (!defined('WWW_ROOT')) {
 
 // for built-in server
 if (php_sapi_name() == 'cli-server') {
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['REQUEST_URI'])) {
+	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['PHP_SELF'])) {
 		return false;
 	}
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
